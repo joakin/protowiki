a61092e77467a959584c38e29f23cea6536e26ce
joakin <joaquin@chimeces.com>
2016-10-25 10:52:26 +0200

    Remove cors proxy since Safari bug was fixed
    https://phabricator.wikimedia.org/T148368

--- src/api.js
+++ src/api.js
@@ -1,15 +1,11 @@
 const HOST = 'en.wikipedia.org'
 
 const restOptions = {
-  mode: 'cors',
-  headers: {}
+  mode: 'cors'
 }
 
-const isSafari = navigator.userAgent.indexOf('Safari') > -1 && navigator.userAgent.indexOf('Chrome') === -1
-const corsProxy = 'https://cors-proxy-mhwanfbyyu.now.sh/'
-
 const rest = (host, endpoint) =>
-  `${isSafari ? corsProxy : ''}https://${host}/api/rest_v1${endpoint}`
+  `https://${host}/api/rest_v1${endpoint}`
 
 const endpoints = {
   article: (title) => `/page/mobile-sections/${title}?redirect=true`
