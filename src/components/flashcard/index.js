4a27d1c9f9f16b9becdbd7d480b9568f3da7f8be
joakin <joaquin@chimeces.com>
2016-11-28 12:36:19 +0100

    Fix flashcard styles (header + footer)
    See https://phabricator.wikimedia.org/T148378#2820804
    
    * Blue link stuck at the bottom
    * Text overflow ellipsis
    * Quickfact logo at the top

--- src/components/flashcard/index.js
+++ src/components/flashcard/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import CaptureClicks from '../capture-clicks'
 import Section from '../section'
 import LeadImage from '../lead-image'
+import Logo from '../logo'
+import quickfactImg from '../images/quickfact.svg'
 
 import './flashcard.css'
 
@@ -16,13 +18,19 @@ export default React.createClass({
   },
 
   render () {
-    const {article, print} = this.props
+    const {title, article, print} = this.props
     const {displaytitle, sections: [lead], image} = article.lead
+    console.log(this.props)
 
     return (
       <CaptureClicks>
 
         <div className='Flashcard'>
+
+          <div className='Flashcard-header'>
+            <Logo size={50} />
+            <img src={quickfactImg} />
+          </div>
 
           <LeadImage style={{ margin: '0 -1em' }} image={image} />
 
@@ -30,7 +38,13 @@ export default React.createClass({
 
           <Section html={lead.text} />
 
-          {print ? <div style={{ opacity: 0 }}>~~PRINT-FINISHED~~</div> : null}
+          <div className='Flashcard-footer'>
+            <a href='https://wikipedia.org/wiki/'>
+              https://wikipedia.org/wiki/{title}
+            </a>
+          </div>
+
+          {print ? <div style={{ height: '1px', opacity: 0 }}>~~PRINT-FINISHED~~</div> : null}
 
         </div>
 
