5836a483fa96231d3eeae65041d339f6527e93ba
joakin <joaquin@chimeces.com>
2017-01-17 11:59:08 -0800

    Add section collapsing

--- src/components/section/index.js
+++ src/components/section/index.js
@@ -1,13 +1,29 @@
 import React from 'react'
 
+import Icon, {types} from '../icon'
+
 import './section.css'
 
-export default function Section ({title, html}) {
-  return (
-    <div className='Section is-open'>
-      {title
-        ? <h2 dangerouslySetInnerHTML={{__html: title}} /> : null}
-      <div className='Section-body' dangerouslySetInnerHTML={{__html: html}} />
-    </div>
-  )
-}
+export default React.createClass({
+  getInitialState () {
+    return {
+      open: true
+    }
+  },
+
+  render () {
+    const {title, html} = this.props
+    const {open} = this.state
+    return (
+      <div className={`Section ${open ? 'is-open' : ''}`}>
+        {title
+          ? <h2 className='Section-title' onClick={(e) => this.setState({ open: !open })}>
+            <Icon type={types.ARROW}
+              className={`Section-collapse ${open ? 'up' : ''}`} />
+            <span dangerouslySetInnerHTML={{__html: title}} />
+          </h2> : null}
+        <div className='Section-body' dangerouslySetInnerHTML={{__html: html}} />
+      </div>
+    )
+  }
+})
